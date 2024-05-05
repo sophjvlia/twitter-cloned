26a95260a0f1b5993ccf49469a647767ba32d707
Sophie Crabtree <111188743+sophjvlia@users.noreply.github.com>
2024-05-05 10:22:36 +0000

    Amendment

--- src/components/AuthProvider.jsx
+++ src/components/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import { auth } from "../firebase";
+import { auth } from "../firebase.js";
 
 export const AuthContext = createContext();
 
